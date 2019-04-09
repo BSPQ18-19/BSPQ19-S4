aff45e5408ab676053fd2905116b4d12ff54bf90
Koldo Urrosolo <koldo.urrosolo@opendeusto.es>
2019-04-09 18:08:02 +0200

    Nuevo modelo de estructura del proyecto (Por el momento se mantendrán los 2)

---
+++ bspq19-s4/src/main/java/cliente/es/deusto/spq/gui/BorrarUsuario.java
@@ -0,0 +1,134 @@
+package cliente.es.deusto.spq.gui;
+
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class BorrarUsuario extends JFrame {
+	private static final long serialVersionUID = -2878465684552734783L;
+	private JPanel contentPane;
+	private JButton btnBorrar;
+	private JList<String> listMostarUsuarios;
+	private static ImageIcon imagen = new ImageIcon("Icono//icono.jpg");
+	private String[] usuarios = {"a","b"};
+
+	public BorrarUsuario() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[] { 0, 0, 0 };
+		gbl_contentPane.rowHeights = new int[] { 0, 0, 0, 0, 0 };
+		gbl_contentPane.columnWeights = new double[] { 1.0, 0.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE };
+		contentPane.setLayout(gbl_contentPane);
+
+		JLabel lblMostrarUsuarios = new JLabel("Seleccione el usuario que desea borrar:");
+		GridBagConstraints gbc_lblMostrarUsuarios = new GridBagConstraints();
+		gbc_lblMostrarUsuarios.gridwidth = 2;
+		gbc_lblMostrarUsuarios.insets = new Insets(0, 0, 5, 0);
+		gbc_lblMostrarUsuarios.gridx = 0;
+		gbc_lblMostrarUsuarios.gridy = 0;
+		contentPane.add(lblMostrarUsuarios, gbc_lblMostrarUsuarios);
+
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridwidth = 2;
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 1;
+		contentPane.add(scrollPane, gbc_scrollPane);
+
+		// TODO: Cargar String[]usuarios con los nombres de todos los usuarios de la BD
+		Arrays.sort(usuarios);
+
+		listMostarUsuarios = new JList<String>(usuarios);
+		listMostarUsuarios.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		listMostarUsuarios.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		scrollPane.setViewportView(listMostarUsuarios);
+
+		listMostarUsuarios.addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent e) {
+				if (listMostarUsuarios.getSelectedIndex() != -1) {
+					btnBorrar.setEnabled(true);
+				}
+			}
+		});
+
+		btnBorrar = new JButton("Borrar");
+		btnBorrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				int i = listMostarUsuarios.getSelectedIndex();
+				String n = usuarios[i];
+				// TODO: eliminar el usuarios con nombre n de la BD
+				setVisible(false);
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							Usuarios frame = new Usuarios();
+							frame.setVisible(true);
+							frame.setTitle("Base de datos de usuarios");
+							frame.setIconImage(imagen.getImage());
+							JOptionPane.showMessageDialog(null, "Usuario eliminado");
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
+		btnBorrar.setEnabled(false);
+		GridBagConstraints gbc_btnBorrar = new GridBagConstraints();
+		gbc_btnBorrar.gridwidth = 2;
+		gbc_btnBorrar.gridx = 0;
+		gbc_btnBorrar.gridy = 2;
+		contentPane.add(btnBorrar, gbc_btnBorrar);
+
+		JButton btnAtras = new JButton("Atr\u00E1s");
+		btnAtras.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				setVisible(false);
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							Usuarios frame = new Usuarios();
+							frame.setVisible(true);
+							frame.setTitle("Base de datos de usuarios");
+							frame.setIconImage(imagen.getImage());
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
+
+		GridBagConstraints gbc_btnAtras = new GridBagConstraints();
+		gbc_btnAtras.gridwidth = 2;
+		gbc_btnAtras.gridx = 0;
+		gbc_btnAtras.gridy = 3;
+		contentPane.add(btnAtras, gbc_btnAtras);
+	}
+
+}
---
+++ bspq19-s4/src/main/java/cliente/es/deusto/spq/gui/VentanaPrincipal.java
@@ -0,0 +1,234 @@
+package cliente.es.deusto.spq.gui;
+
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+//import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class VentanaPrincipal extends JFrame {
+	private static final long serialVersionUID = 8689513507453358451L;
+	private JPanel contentPane;
+	private JLabel lblUsuario;
+	private JTextField textFieldUsuario;
+	private JLabel lblContrasea;
+	private JPasswordField passwordFieldContrasea;
+	private JButton btnNuevoUsuario;
+	private JButton btnContraseaOlvidada;
+	private JButton btnIniciarSesion;
+	static boolean usuario = false;
+	static boolean administrador = false;
+	static String nombre = null;
+	private static ImageIcon imagen = new ImageIcon("Icono//icono.jpg");
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPrincipal frame = new VentanaPrincipal();
+					frame.setVisible(true);
+					frame.setTitle("Login");
+					frame.setIconImage(imagen.getImage());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public VentanaPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 500, 350);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[] { 0, 0, 0 };
+		gbl_contentPane.rowHeights = new int[] { 0, 0, 0, 0, 0 };
+		gbl_contentPane.columnWeights = new double[] { 1.0, 1.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE };
+		contentPane.setLayout(gbl_contentPane);
+
+		lblUsuario = new JLabel("Usuario:");
+		GridBagConstraints gbc_lblUsuario = new GridBagConstraints();
+		gbc_lblUsuario.fill = GridBagConstraints.VERTICAL;
+		gbc_lblUsuario.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUsuario.gridx = 0;
+		gbc_lblUsuario.gridy = 0;
+		contentPane.add(lblUsuario, gbc_lblUsuario);
+
+		textFieldUsuario = new JTextField();
+		GridBagConstraints gbc_textFieldUsuario = new GridBagConstraints();
+		gbc_textFieldUsuario.insets = new Insets(0, 0, 5, 0);
+		gbc_textFieldUsuario.gridx = 1;
+		gbc_textFieldUsuario.gridy = 0;
+		contentPane.add(textFieldUsuario, gbc_textFieldUsuario);
+		textFieldUsuario.setColumns(20);
+
+		textFieldUsuario.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					nombre = textFieldUsuario.getText();
+					// TODO: combrobar si nombre est� en la BD
+//					if(nombre est� en la BD y es administrador (privilegio==1)){
+//						administrador = true;
+//						lblContrasea.setEnabled(true);
+//						passwordFieldContrasea.setEnabled(true);
+//						btnContraseaOlvidada.setEnabled(true);
+//						passwordFieldContrasea.requestFocus();
+//						textFieldUsuario.setEditable(false);
+//					}else if(nombre est� en la BD y es usuario (privilegio==0))
+//						usuario = true;
+//						lblContrasea.setEnabled(true);
+//						passwordFieldContrasea.setEnabled(true);
+//						btnContraseaOlvidada.setEnabled(true);
+//						passwordFieldContrasea.requestFocus();
+//						textFieldUsuario.setEditable(false);
+//					}
+					if (administrador == false && usuario == false) {
+						JOptionPane.showMessageDialog(null, "El usuario introducido no est\u00E1 registrado");
+					}
+				}
+			}
+		});
+
+		lblContrasea = new JLabel("Contrase\u00F1a:");
+		lblContrasea.setEnabled(false);
+		GridBagConstraints gbc_lblContrasea = new GridBagConstraints();
+		gbc_lblContrasea.insets = new Insets(0, 0, 5, 5);
+		gbc_lblContrasea.gridx = 0;
+		gbc_lblContrasea.gridy = 1;
+		contentPane.add(lblContrasea, gbc_lblContrasea);
+
+		passwordFieldContrasea = new JPasswordField();
+		passwordFieldContrasea.setEnabled(false);
+		passwordFieldContrasea.setColumns(20);
+		GridBagConstraints gbc_passwordFieldContrasea = new GridBagConstraints();
+		gbc_passwordFieldContrasea.insets = new Insets(0, 0, 5, 0);
+		gbc_passwordFieldContrasea.gridx = 1;
+		gbc_passwordFieldContrasea.gridy = 1;
+		contentPane.add(passwordFieldContrasea, gbc_passwordFieldContrasea);
+
+		passwordFieldContrasea.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					String contrasenya = new String(passwordFieldContrasea.getPassword());
+					// TODO: comprobar contrasenya correcta para nombre
+//					if(contrase�a correcta){
+//						btnIniciarSesion.setEnabled(true);
+//						passwordFieldContrasea.setEnabled(false);
+//						btnIniciarSesion.requestFocus();
+//					}else{
+//						JOptionPane.showMessageDialog(null, "Contrase\u00F1a incorrecta");
+//					}
+				}
+			}
+		});
+
+		btnNuevoUsuario = new JButton("Registrar nuevo usuario");
+		btnNuevoUsuario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				setVisible(false);
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							RegistrarNuevoUsuario frame = new RegistrarNuevoUsuario();
+							frame.setVisible(true);
+							frame.setTitle("Registrar nuevo usuario");
+							frame.setIconImage(imagen.getImage());
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
+		GridBagConstraints gbc_btnNuevoUsuario = new GridBagConstraints();
+		gbc_btnNuevoUsuario.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNuevoUsuario.gridx = 0;
+		gbc_btnNuevoUsuario.gridy = 2;
+		contentPane.add(btnNuevoUsuario, gbc_btnNuevoUsuario);
+
+		btnContraseaOlvidada = new JButton("Se me ha olvidado la contrase\u00F1a");
+		btnContraseaOlvidada.setEnabled(false);
+		btnContraseaOlvidada.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							ContrasenyaOlvidada frame = new ContrasenyaOlvidada();
+							frame.setVisible(true);
+							frame.setTitle("Contrase\u00F1a olvidada");
+							frame.setIconImage(imagen.getImage());
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
+		GridBagConstraints gbc_btnContraseaOlvidada = new GridBagConstraints();
+		gbc_btnContraseaOlvidada.insets = new Insets(0, 0, 5, 0);
+		gbc_btnContraseaOlvidada.gridx = 1;
+		gbc_btnContraseaOlvidada.gridy = 2;
+		contentPane.add(btnContraseaOlvidada, gbc_btnContraseaOlvidada);
+
+		btnIniciarSesion = new JButton("Iniciar Sesi\u00F3n");
+		btnIniciarSesion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				setVisible(false);
+				if (usuario == true) {
+					EventQueue.invokeLater(new Runnable() {
+						public void run() {
+							try {
+								Usuario frame = new Usuario();
+								frame.setVisible(true);
+								frame.setTitle("Usuario");
+								frame.setIconImage(imagen.getImage());
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					});
+				} else if (administrador == true) {
+					EventQueue.invokeLater(new Runnable() {
+						public void run() {
+							try {
+								Administrador frame = new Administrador();
+								frame.setVisible(true);
+								frame.setTitle("Administrador");
+								frame.setIconImage(imagen.getImage());
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					});
+				}
+			}
+		});
+		btnIniciarSesion.setEnabled(false);
+		GridBagConstraints gbc_btnIniciarSesion = new GridBagConstraints();
+		gbc_btnIniciarSesion.gridwidth = 2;
+		gbc_btnIniciarSesion.insets = new Insets(0, 0, 0, 5);
+		gbc_btnIniciarSesion.gridx = 0;
+		gbc_btnIniciarSesion.gridy = 3;
+		contentPane.add(btnIniciarSesion, gbc_btnIniciarSesion);
+	}
+
+}
