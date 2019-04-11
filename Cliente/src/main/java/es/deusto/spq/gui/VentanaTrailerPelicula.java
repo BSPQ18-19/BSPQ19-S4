66ac8628889ce56a152f49195e01fd3677e8efe0
byJonMartinez <jonmartinezq@opendeusto.es>
2019-04-11 11:24:40 +0200

    Código actualizado pero las librerias son de 32 bits (Son necesarias de 64 bits)

--- Cliente/src/main/java/es/deusto/spq/gui/VentanaTrailerPelicula.java
+++ Cliente/src/main/java/es/deusto/spq/gui/VentanaTrailerPelicula.java
@@ -7,36 +7,52 @@ import java.net.URL;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 
+import chrriis.dj.nativeswing.swtimpl.NativeInterface;
+import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
+
 public class VentanaTrailerPelicula extends JFrame {
+	public VentanaTrailerPelicula() {
+	}
 
 	private JPanel contentPane;
 
 
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaTrailerPelicula frame = new VentanaTrailerPelicula();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+		NativeInterface.open();
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run(){
+				JFrame frame = new JFrame("Trailer de la pelicula");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.getContentPane().add(VentanaTrailerPelicula(), BorderLayout.CENTER);
+				frame.setSize(1024, 768);
+				frame.setVisible(true);
 			}
 		});
+		
+		NativeInterface.runEventPump();
+		
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+
+			public void run() {
+				NativeInterface.close();
+			}
+			
+		}));
 	}
 
-	
-	public VentanaTrailerPelicula() {
-		setTitle("Trailer");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 910, 497);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-		
+	//Método que devuelve el panel con tráiler
+	public static JPanel VentanaTrailerPelicula() {
+		JPanel PanelReproductor = new JPanel(new BorderLayout());
+		JWebBrowser panel = new JWebBrowser();
+		PanelReproductor.add(panel, BorderLayout.CENTER);
+		panel.setBarsVisible(false);
+		panel.navigate("https://www.youtube.com/watch?v=Q4_ex7a9ZcY");
+		//Utilizo watch_popuppara poder ver el trailer en pantalla completa
+		return PanelReproductor;
 		
 	}
 
