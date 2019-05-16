c8d6145eebb318e9b0cebfca7a825ed1687d4203
lucia-uribe <lucia.uribe@opendeusto.es>
2019-05-16 23:20:05 +0200

    Error en paso de titulo

--- src/main/java/cliente/es/deusto/spq/gui/Pelicula.java
+++ src/main/java/cliente/es/deusto/spq/gui/Pelicula.java
@@ -18,7 +18,7 @@ public class Pelicula extends JPanel{
 
 	private static final long serialVersionUID = 674330126384087764L;
 	 
-	static JLabel lblGetTit;
+	static String titulo;
 	public Pelicula(PeliculaController peliculaController, CardLayout cardLayout) {
 		
 		GridBagLayout gbl_contentPane = new GridBagLayout();
@@ -39,7 +39,7 @@ public class Pelicula extends JPanel{
 		gbc_lblTitulo.gridy = 0;
 		add(lblTitulo, gbc_lblTitulo);
 		
-		lblGetTit = new JLabel(BuscarPelicula.pelicula);
+		JLabel lblGetTit = new JLabel(titulo);
 		GridBagConstraints gbc_lblGetTit = new GridBagConstraints();
 		gbc_lblGetTit.fill = GridBagConstraints.BOTH;
 		gbc_lblGetTit.insets = new Insets(0, 0, 5, 0);
